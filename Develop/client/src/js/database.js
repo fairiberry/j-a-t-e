cae39310af108593085d4959c4872a937f29dcde
Farrah Syed <107257012+fairiberry@users.noreply.github.com>
2022-09-22 10:33:23 -0400

    Add files via upload

---
+++ Develop/client/src/js/database.js
@@ -0,0 +1,34 @@
+import { openDB } from 'idb';
+
+const initdb = async () =>
+  openDB('jate', 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains('jate')) {
+        console.log('jate database already exists');
+        return;
+      }
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('jate database created');
+    },
+  });
+
+// TODO: Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => { console.error('putDb not implemented');
+const jateDB = openDB("jate", 1);
+const text = jateDB.transaction("jate", "readonly");
+const store= text.objectStore("jate");
+const request = store.put({id: 1, value: content });
+const results = request;
+};
+
+
+// TODO: Add logic for a method that gets all the content from the database
+export const getDb = async () => {
+  console.error('getDb not implemented');
+  const jateDB = openDB("jate", 1);
+  const text = jateDB.transaction("jate", "readonly");
+  const store= text.objectStore("jate");
+  const request = store.getAll();
+  return request.content;
+};
+initdb();
---
+++ Develop/client/webpack.config.js
@@ -0,0 +1,38 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const path = require('path');
+const { InjectManifest } = require('workbox-webpack-plugin');
+
+// TODO: Add and configure workbox plugins for a service worker and manifest file.
+// TODO: Add CSS loaders and babel to webpack.
+
+module.exports = () => {
+  return {
+    mode: 'development',
+    entry: {
+      main: './src/js/index.js',
+      install: './src/js/install.js'
+    },
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./index.html"
+      }),
+      new WebpackPwaManifest({
+        name: "J.A.T.E.",
+        description: "Just another text editor~!",
+        start_url: "/",
+        icon: "/Develop/client/src/images/logo.png"
+      })
+    ],
+
+    module: {
+      rules: [
+        
+      ],
+    },
+  };
+};
